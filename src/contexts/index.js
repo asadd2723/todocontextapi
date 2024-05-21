2f5fa6a5357f8792c066103e5e3e6bef937b8c5f
asadd2723 <asadu3040@gmail.com>
2024-05-21 11:31:35 +0500

    make todoContext file and add new export file for todoContext

---
+++ src/contexts/index.js
@@ -0,0 +1,3 @@
+// yeh file sirf context k value ko export krna k liya simple idr sy export krdo context ki value ko 
+
+export {todoContext,todoProvider,useTodo} from './todoContext'
--- src/contexts/todoContext.js
+++ src/contexts/todoContext.js
@@ -1,17 +1,23 @@
 import { createContext,useContext } from "react";
 
+//createContext
 export const todoContext = createContext({
-  todo:[
+  todos:[
     {
       id:1,
-      todo:"todo msg",
-      completed: false,
+      todoMsg: "Todo msg",
+      completed:false
     }
   ],
+  addTodo:(todoMsg)=>{},
+  updatedTodo:(id,todoMsg)=>{},
+  deleteTodo:(id)=>{},
+  toggleComplete: (id)=>{}
 })
-
+//context provider
 export const todoProvider = todoContext.Provider
 
-export const  useTodo = ()=>{
+//custom hooks 
+export const useTodo = ()=>{
   return useContext(todoContext)
 }
